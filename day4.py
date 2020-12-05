fb4240957550754c169ee11521e855616aea5011
Ilyes NOUMRI <ilyes.noumri@epita.fr>
2020-12-05 23:06:20 +0100

    day4: using getattribute rather than eval

--- day4.py
+++ day4.py
@@ -1,21 +1,23 @@
 from lib import open_file
 import re
 
-_input = open_file("4/input")
+f = open_file("4/input")
 # We add an ending empty string just for conveniance
-_input.append("")
+f.append("")
 passports = []
 passport = {}
-for elt in _input:
+for elt in f:
     if elt:
-        _elt = elt.split()
-        for __elt in _elt:
-            key, value = __elt.split(":")
-            passport[key] = value
+        passport_elts = elt.split()
+        for passport_elt in passport_elts:
+            key, value = passport_elt.split(":")
+            passport[key] = str(value)
     else:
         passports.append(passport)
         passport = {}
+
 required_fields = ("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
+ecl_possibilities = ("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
 
 
 def digits_regstring(n=4, add=""):
@@ -29,38 +31,36 @@ def digits_regexp(n=4, add=""):
     return re.compile(digits_regstring(n, add))
 
 
-def byr(s):
-    return digits_regexp().match(s) and int(s) >= 1920 and int(s) <= 2002
+class Checker():
+    def byr(self, s):
+        return digits_regexp().match(s) and int(s) >= 1920 and int(s) <= 2002
+
+    def iyr(self, s):
+        return digits_regexp().match(s) and int(s) >= 2010 and int(s) <= 2020
+
+    def eyr(self, s):
+        return digits_regexp().match(s) and int(s) >= 2020 and int(s) <= 2030
+
+    def hgt(self, s):
+        if digits_regexp(3, "cm").match(s):
+            num = s.strip("cm")
+            return int(num) >= 150 and int(num) <= 193
+        if digits_regexp(2, "in").match(s):
+            num = s.strip("in")
+            return int(num) >= 59 or int(num) <= 76
+        return False
+
+    def hcl(self, s):
+        return re.compile(r"^#([0-9]|[a-f]){6}$").match(s)
+
+    def ecl(self, s):
+        return s in ecl_possibilities
+
+    def pid(self, s):
+        return digits_regexp(9).match(s)
 
 
-def iyr(s):
-    return digits_regexp().match(s) and int(s) >= 2010 and int(s) <= 2020
-
-
-def eyr(s):
-    return digits_regexp().match(s) and int(s) >= 2020 and int(s) <= 2030
-
-
-def hgt(s):
-    if digits_regexp(3, "cm").match(s):
-        num = s.strip("cm")
-        return int(num) >= 150 and int(num) <= 193
-    if digits_regexp(2, "in").match(s):
-        num = s.strip("in")
-        return int(num) >= 59 or int(num) <= 76
-    return False
-
-
-def hcl(s):
-    return re.compile(r"^#([0-9]|[a-f]){6}$").match(s)
-
-
-def ecl(s):
-    return s in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
-
-
-def pid(s):
-    return digits_regexp(9).match(s)
+checker = Checker()
 
 
 def valid_passport(passport, hard_check=False):
@@ -71,8 +71,7 @@ def valid_passport(passport, hard_check=False):
             return False
         if hard_check:
             arg = passport[field]
-            call = f'{field}("{arg}")'
-            if not eval(call):
+            if not checker.__getattribute__(field)(arg):
                 return False
     return True
 
